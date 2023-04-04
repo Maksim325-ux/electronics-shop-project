384fce0266e2d88a84cdf48b2ffadf32f66e7427
Max <maks_orlov_2015@mail.ru>
2023-04-04 19:34:34 +1000

    задание 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+@pytest.fixture
+def item1():
+    return Item("Смартфон", 10000, 20)
+def test_init(item1):
+    assert item1.name == 'Смартфон'
+    assert item1.price == 10000
+    assert item1.quantity == 20
+
+
+def test_calculate_total_price(item1):
+    assert total_price == 200000
